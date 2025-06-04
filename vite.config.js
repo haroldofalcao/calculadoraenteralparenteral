d587f4bc047843c4320e9fd67159259431e1231a
jpedro002 <joaopsilvavolei@gmail.com>
2025-06-04 19:14:32 -0300

    fix 2

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,7 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    
   },
   server: {
     port: 3000,
